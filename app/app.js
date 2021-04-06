1866b08279013d53271befd8fca98327daa637e4
JoosoebKim <outfox100@naver.com>
2021-04-06 23:28:57 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // 모듈
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 //라우팅
@@ -12,6 +13,8 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json()); // bodyParser가 json 데이터를 가져올 수 있도록 명시
+app.use(bodyParser.urlencoded({ extended: true })) // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use("/", home); // use => 미들웨어를 등록해주는 메서드. app.get 과는 달리 라우팅 부분이기 때문에 미들웨어로 등록하는 것이다.
 
 module.exports = app;
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,7 +5,8 @@ const router = express.Router(); // 라우터 부분이기 때문에 app 이 아
 
 const ctrl = require('./home.ctrl');
 
-router.get('/', ctrl.home);
-router.get('/login', ctrl.login);
+router.get('/', ctrl.output.home);
+router.get('/login', ctrl.output.login);
+router.post('/login', ctrl.process.login);
 
 module.exports = router;
