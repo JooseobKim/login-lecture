3a6026bdbfd4a1e9f78c159fbabeccd9ceeb11ca
JoosoebKim <outfox100@naver.com>
2021-04-08 17:42:14 +0900

    백엔드 맛보기 최종 최적화

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const router = express.Router(); // 라우터 부분이기 때문에 app 이 아니라 router를 불러오는 것이다.
-
 const ctrl = require('./home.ctrl');
 
 router.get('/', ctrl.output.home);
